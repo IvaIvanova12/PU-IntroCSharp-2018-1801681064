4f6fccfa14f8c072c0689695cbd04530fc93b6ba
IvaIvanova12 <ivka974@abv.bg>
2018-11-10 21:56:34 +0200

    Included homework for week 6.

--- PUIntroHomeworks/BusTicketLuckyNumbers/Program.cs
+++ PUIntroHomeworks/BusTicketLuckyNumbers/Program.cs
@@ -11,7 +11,8 @@ namespace BusTicketLuckyNumbers
             int unluckyTicketsCount = 0;
             for (int i = 0; i < 1000000; i++)
             {
-                if ((i / 100000 + i % 100000 / 10000 + i % 100000 % 10000 / 1000) == (i % 100000 % 10000 % 1000 / 100 + i % 100000 % 10000 % 1000 % 100 / 10 + i % 10))
+                if ((i / 100000 + i % 100000 / 10000 + i % 100000 % 10000 / 1000) ==
+                    (i % 100000 % 10000 % 1000 / 100 + i % 100000 % 10000 % 1000 % 100 / 10 + i % 10))
                 {
                     Console.WriteLine(i);
                     luckyTicketsCount++;
@@ -21,8 +22,6 @@ namespace BusTicketLuckyNumbers
                     unluckyTicketsCount++;
                 }
             }
-
-           
             Console.WriteLine("The number of lucky tickets is: " + luckyTicketsCount);
             Console.WriteLine("The number of unlucky tickets is: " + unluckyTicketsCount);
             Console.WriteLine($"Total tickets: {luckyTicketsCount + unluckyTicketsCount}");
---
+++ PUIntroHomeworks/CalculatingName/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+
+namespace CalculatingName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.WriteLine("");
+
+            string[] lines = System.IO.File.ReadAllLines(@"D:\VS_works\PU-IntroCSharp-2018-1801681064\PUIntroHomeworks\StudentsNames.txt");
+
+            // Display the file contents by using a foreach loop.
+            System.Console.WriteLine("Contents of StudentsNames.txt = ");
+
+            int tStudents = 0;
+
+            foreach (string line in lines)
+            {
+                string[] elements = line.Split(' ');
+                string firstName = elements[1];
+                string name = $"{firstName}{elements[3]}";
+                // Use a tab to indent each line of the file.
+                //Console.WriteLine("\t" + line);
+
+                int sum = 0;
+                for (int i = 0; i < firstName.Length; i++)
+                {
+                    sum += (int)firstName[i];
+                }
+
+                if (sum > 10000)
+                {
+                    tStudents += 1;
+                    Console.WriteLine($"{firstName} {elements[3]} => {firstName.Length} => {(char)0x263A}");
+                }
+            }
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            System.Console.ReadKey();
+        }
+    }
+}
